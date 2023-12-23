4b46c341419018f597acf5c487400bf33145c233
Charley <33134502+twist3dimages@users.noreply.github.com>
2023-12-23 17:16:51 -0500

    Update logdb.py

--- ydl_server/logdb.py
+++ ydl_server/logdb.py
@@ -225,20 +225,8 @@ class JobsDB:
             (str(pid), str(job_id)),
         )
         self.conn.commit()
-    # def set_job_log(self, job_id, log):
-    #     truncated_log = log[-2500:] if len(log) > 2500 else log
-    #     cursor = self.conn.cursor()
-    #     cursor.execute(
-    #         """
-    #         UPDATE jobs
-    #         SET log = %s, last_update = NOW()
-    #         WHERE id = %s;
-    #         """,
-    #         (truncated_log, job_id),
-    #     )
-    #     self.conn.commit()
-
     def set_job_log(self, job_id, log):
+        truncated_log = log[-2500:] if len(log) > 2500 else log
         cursor = self.conn.cursor()
         cursor.execute(
             """
@@ -246,9 +234,20 @@ class JobsDB:
             SET log = %s, last_update = NOW()
             WHERE id = %s;
             """,
-            (log, job_id)  # Use the full log directly
+            (truncated_log, job_id),
         )
         self.conn.commit()
+#    def set_job_log(self, job_id, log):
+##        cursor = self.conn.cursor()
+ #       cursor.execute(
+#            """
+#            UPDATE jobs
+#            SET log = %s, last_update = NOW()
+#            WHERE id = %s;
+#            """,
+#            (log, job_id)  # Use the full log directly
+#        )
+#        self.conn.commit()
     
 
 
